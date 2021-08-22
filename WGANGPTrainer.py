c2d52d38e97ec0212bc8488c454d84a23a3e2300
WHDaniels <whdaniels3@gmail.com>
2021-08-22 14:45:17 -0500

    refactoring

--- WGANGPTrainer.py
+++ WGANGPTrainer.py
@@ -36,106 +36,30 @@ class WGANGPTrainer(BaseTrainer):
             self.total_loss_list = list()
 
             for i, batch in enumerate(self.loader):
-                # print(len(self.loader))
                 print(f"Epoch {epoch} | Iter {i}")
 
                 start = perf_counter()
 
                 # get batch data (multi_gpu assumed)
-                # device 2 below
                 first_B = torch.unsqueeze(batch['B_rgb'][0], 0).to(self.device1)
-
                 rest_A_grey = [torch.unsqueeze(image, 0) for image in batch['A_greyscale'][1:]]
                 rest_B_rgb = [torch.unsqueeze(image, 0) for image in batch['B_rgb'][1:]]
-                # rest_A_grey = [torch.unsqueeze(image, 0) for image in batch['A_greyscale'][1:]]
 
                 rest = zip(rest_A_grey, rest_B_rgb)
 
                 # Train generator
+                # Please re-enable generator saving when training generator
+                gen_error = self.train_generator(first_B)
 
-                """
-                Please re-enable generator saving when training generator
-                
-                for param in self.critic.parameters():
-                    param.requires_grad = False
-                    
-                self.gen.zero_grad()
-                self.fake_output = self.gen(first_B)
-                critic_generated = self.critic(self.fake_output)
-                gen_cost = - critic_generated.mean()
-                print('gen_cost', gen_cost)
-                gen_cost.backward()
-                self.gen_optimizer.step()
-                """
-
-                for param in self.critic.parameters():
-                    param.requires_grad = True
-
-                total_critic_loss = 0
-                for A, B in rest:
-                    self.critic.zero_grad()
-                    real_output = A.to(self.device1)  # greyscale real output
-                    # device 2 below
-                    real_input = B.to(self.device1)  # colored real input
-
-                    self.critic.zero_grad()
-
-                    with torch.no_grad():
-                        # real_input here is 3-channel image
-                        gen_input = real_input  # totally freeze G, training D
-                    self.fake_output = self.gen(gen_input).detach()
-
-                    # if training B2A:
-                    # real_output here should be a 1-channel greyscale image (take 1-channel version of image from dataloader)
-                    real_pred = self.critic(real_output)
-
-                    # and fake_B should be a 1-channel greyscale image (set ColorNet(dim=1), not hard)
-                    fake_pred = self.critic(self.fake_output)
-
-                    # calculate gradient penalty
-                    # here want 1-channel version of real_input as well, to match fake_B
-                    ### gradient_penalty = self.calc_gradient_penalty(real_output, self.fake_output)
-
-                    alpha = torch.rand(self.args.batch_size, 1)
-                    alpha = alpha.expand(self.args.batch_size,
-                                         int(real_output.nelement() / self.args.batch_size)).contiguous()
-                    alpha = alpha.view(self.args.batch_size, self.channels, self.args.image_size, self.args.image_size)
-                    alpha = alpha.to(self.device1)
-
-                    # fake_data = self.fake_output.view(self.args.batch_size, self.channels, self.args.image_size, self.args.image_size)
-                    # interpolated = alpha * real_data.detach() + ((1 - alpha) * fake_data.detach())
-                    interpolated = alpha * real_output + ((1 - alpha) * self.fake_output)
-                    interpolated = interpolated.to(self.device1)
-                    interpolated.requires_grad_(True)
-
-                    interpolated = autograd.Variable(interpolated, requires_grad=True)
-                    critic_interpolated = self.critic(interpolated)
-
-                    gradients = autograd.grad(outputs=critic_interpolated, inputs=interpolated,
-                                              grad_outputs=torch.ones(critic_interpolated.size()).to(self.device1),
-                                              create_graph=True, retain_graph=True, only_inputs=True)[0]
-
-                    gradients = gradients.view(real_output.size(0), -1)
-                    gradient_penalty = (((gradients + 1e-16).norm(2, dim=1) - 1.0) ** 2).mean() * 10
-
-                    # final critic cost
-                    critic_cost = fake_pred.mean() - real_pred.mean() + gradient_penalty
-                    critic_cost.backward()
-
-                    print("total", critic_cost)
-                    print("fake", fake_pred.mean())
-                    print("real_pred", real_pred.mean())
-                    print("both", fake_pred.mean() - real_pred.mean())
-                    print("gp", gradient_penalty)
-                    print("\n")
-
-                    self.critic_optimizer.step()
-                    total_critic_loss += critic_cost.cpu().detach().item()
+                # Train critic
+                total_critic_loss = self.train_critic(rest)
 
                 # if training both
-                ## self.loss_list.append((gen_cost.detach().item(), total_critic_loss / self.args.critic_iters))
+                self.loss_list.append((gen_error.detach().item(), total_critic_loss / self.args.critic_iters))
+
                 # if training critic only
-                self.loss_list.append((0, total_critic_loss / self.args.critic_iters))
+                # self.loss_list.append((0, total_critic_loss / self.args.critic_iters))
+
                 self.plot_metrics(epoch, i)
 
                 real_input, real_output = list(zip(rest_A_grey, rest_B_rgb))[-1]
@@ -147,9 +71,90 @@ class WGANGPTrainer(BaseTrainer):
 
         self.final_save()
 
+    def train_generator(self, first_B):
+        for param in self.critic.parameters():
+            param.requires_grad = False
+
+        self.gen.zero_grad()
+        self.fake_output = self.gen(first_B)
+        critic_generated = self.critic(self.fake_output)
+        gen_cost = - critic_generated.mean()
+        print('gen_cost', gen_cost)
+        gen_cost.backward()
+        self.gen_optimizer.step()
+
+        return gen_cost
+
+    def calculate_gradient_penalty(self, real_output):
+        alpha = torch.rand(self.args.batch_size, 1)
+        alpha = alpha.expand(self.args.batch_size,
+                             int(real_output.nelement() / self.args.batch_size)).contiguous()
+        alpha = alpha.view(self.args.batch_size, self.channels, self.args.image_size, self.args.image_size)
+        alpha = alpha.to(self.device1)
+
+        interpolated = alpha * real_output + ((1 - alpha) * self.fake_output)
+        interpolated = interpolated.to(self.device1)
+        interpolated.requires_grad_(True)
+
+        interpolated = autograd.Variable(interpolated, requires_grad=True)
+        critic_interpolated = self.critic(interpolated)
+
+        gradients = autograd.grad(outputs=critic_interpolated, inputs=interpolated,
+                                  grad_outputs=torch.ones(critic_interpolated.size()).to(self.device1),
+                                  create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+        gradients = gradients.view(real_output.size(0), -1)
+        gradient_penalty = (((gradients + 1e-16).norm(2, dim=1) - 1.0) ** 2).mean() * 10
+
+        return gradient_penalty
+
+    def train_critic(self, rest):
+        for param in self.critic.parameters():
+            param.requires_grad = True
+
+        total_critic_loss = 0
+        for A, B in rest:
+            self.critic.zero_grad()
+            real_output = A.to(self.device1)  # greyscale real output
+            real_input = B.to(self.device1)  # colored real input
+
+            self.critic.zero_grad()
+
+            with torch.no_grad():
+                # real_input here is 3-channel image
+                gen_input = real_input  # totally freeze G, training D
+            self.fake_output = self.gen(gen_input).detach()
+
+            # if training B2A:
+            # real_output here should be a 1-channel greyscale image (take 1-channel version of image from dataloader)
+            real_pred = self.critic(real_output)
+
+            # and fake_B should be a 1-channel greyscale image (set ColorNet(dim=1), not hard)
+            fake_pred = self.critic(self.fake_output)
+
+            # calculate gradient penalty
+            # here want 1-channel version of real_input as well, to match fake_B
+            gradient_penalty = self.calculate_gradient_penalty(real_output)
+
+            # final critic cost
+            critic_cost = fake_pred.mean() - real_pred.mean() + gradient_penalty
+
+            critic_cost.backward()
+
+            print("total", critic_cost)
+            print("fake", fake_pred.mean())
+            print("real_pred", real_pred.mean())
+            print("both", fake_pred.mean() - real_pred.mean())
+            print("gp", gradient_penalty)
+            print("\n")
+
+            self.critic_optimizer.step()
+            total_critic_loss += critic_cost.cpu().detach().item()
+
+            return total_critic_loss
+
     def initialize_nets(self):
         if self.args.multi_gpu:
-            # device 2 below
             self.gen = Generator(3, 1).to(self.device1)
             # self.gen = ColorNet(self.channels).to(self.device1)
             # self.critic = Discriminator(self.channels).to(self.device1)
@@ -170,7 +175,7 @@ class WGANGPTrainer(BaseTrainer):
             self.critic.load_state_dict(torch.load(self.args.dis_A_dict))
 
     def define_optimizers(self):
-        self.gen_optimizer = torch.optim.Adam(self.gen.parameters(), lr=self.args.lr / 10, betas=(0.5, 0.999))
+        self.gen_optimizer = torch.optim.Adam(self.gen.parameters(), lr=self.args.lr / 4, betas=(0.5, 0.999))
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.args.lr, betas=(0.5, 0.999))
 
     def define_schedulers(self):
@@ -186,7 +191,7 @@ class WGANGPTrainer(BaseTrainer):
         """
         tensor_dict = {}
 
-        if i % 50 == 0:
+        if i % 5 == 0:
             # show results on test per epoch
             try:
                 os.mkdir(os.getcwd() + f'\\results\\epoch_{epoch}')
